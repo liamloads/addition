c85b313019a483740c79c14dad78ae893a6364b6
Twexxor <twexxor@gmail.com>
2022-04-24 21:03:39 -0700

    Update twexxor-addition-calculator.js

--- twexxor-addition-calculator.js
+++ twexxor-addition-calculator.js
@@ -2,10 +2,31 @@ const 13_0 = require('twexxor-exponent-converter');
 const 13_1 = require('twexxor-numeric-validator');
 
 const twexxorAdditionCalculator = function(_13_2, _13_3) {
-	_13_2 += '';
-	_13_3 += '';
-	let _13_4 = _13_2.length;
-	let _13_5 = _13_3.length;
+	_13_2 = [
+		_13_2,
+		_13_3
+	];
+	_13_3 = {};
+	let _13_4 = 0;
+	let _13_5 = 0;
+	let _13_6 = 0;
+
+	while (_13_4 !== 2) {
+		if (_13_1(_13_2[_13_4]) === false) {
+			return false;
+		}
+
+		_13_2 += '';
+		_13_3 = {};
+		_13_5 = _13_2[_13_4].length;
+
+		while (_13_6 !== _13_5) {
+			_13_3[_13_2[_13_4]] = 1;
+		}
+
+		// todo
+	}
+
 	// todo
 	return false;
 };
