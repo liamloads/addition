7f59dae8974c588d6624f1e8f066a1017a5ae80d
Twexxor <twexxor@gmail.com>
2022-04-24 20:12:12 -0700

    Create twexxor-addition-calculator.js

---
+++ twexxor-addition-calculator.js
@@ -0,0 +1,15 @@
+const 13_0 = require('twexxor-exponent-converter');
+const 13_1 = require('twexxor-numeric-validator');
+
+const twexxorAdditionCalculator = function(_13_2, _13_3) {
+	_13_2 += '';
+	_13_3 += '';
+	let _13_4 = _13_2.length;
+	let _13_5 = _13_3.length;
+	// todo
+	return false;
+};
+
+if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
+	module.exports = twexxorAdditionCalculator;
+}
