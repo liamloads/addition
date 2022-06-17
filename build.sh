4bf9136ca1df70fdee49db0e2f3a7454066c14f4
Twexxor <twexxor@gmail.com>
2022-06-17 09:16:41 -0700

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,5 @@
+gcc -o twexxor-addition-calculator addition-calculator.c
+git clone https://github.com/twexxor/digit-validator.git
+gcc -o twexxor-digit-validator digit-validator/digit-validator.c
+git clone https://github.com/twexxor/string-repeater.git
+gcc -o twexxor-string-repeater string-repeater/string-repeater.c
